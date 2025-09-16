5bd2e34cb316cc50f964f957fe8dc128f4a37b56
Anvesh jain <anveshjainj@gmail.com>
2025-09-16 22:47:54 +0530

    02.Setup better-auth and Conectivity Databses Complete

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,14 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
-  dialect: 'postgresql',
+  schema: "./src/db/schema.ts",   // apni schema file ka path
+  out: "./drizzle",                // migrations output folder
+  dialect: "postgresql",           // 👈 yeh important hai
+  
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,  // env se DB URL
   },
 });
